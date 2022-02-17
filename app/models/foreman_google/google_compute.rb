1725b10ea05c0d5d0a4d9c7931abd1e73579cac3
Ondřej Ezr <oezr@redhat.com>
2022-02-17 22:25:36 +0100

    Scaffold the ComputeResource implementation

---
+++ app/models/foreman_google/google_compute.rb
@@ -0,0 +1,42 @@
+module ForemanGoogle
+  class GoogleCompute
+    def persisted?
+    end
+
+    def ready?
+    end
+
+    def reload
+    end
+
+    def state
+    end
+
+    def start
+    end
+
+    def stop
+    end
+
+    def to_s
+    end
+
+    def interfaces
+    end
+
+    def interfaces_attributes=(attrs)
+    end
+
+    def volumes
+    end
+
+    def volumes_attributes=(attrs)
+    end
+
+    def identity
+    end
+
+    def identity=(setuuid)
+    end
+  end
+end
