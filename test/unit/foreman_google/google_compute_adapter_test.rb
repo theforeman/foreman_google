1ae18673c3edd04704f006fdf88842d6adae8b8a
Leos Stejskal <lstejska@redhat.com>
2022-03-09 14:14:14 +0000

    ForemanGoogle::GCE #images

--- test/unit/foreman_google/google_compute_adapter_test.rb
+++ test/unit/foreman_google/google_compute_adapter_test.rb
@@ -55,5 +55,42 @@ module ForemanGoogle
         assert_equal ['machine_type_001'], subject.machine_types('us-east1-b').map(&:name)
       end
     end
+
+    describe '#images' do
+      setup do
+        stub_request(:get, 'https://compute.googleapis.com/compute/v1/projects/coastal-haven-123456/global/images')
+          .to_return(body: File.read(File.join(__dir__, '..', '..', 'fixtures', 'images_coastal.json')))
+      end
+
+      it 'list images' do
+        stub_request(:get, 'https://compute.googleapis.com/compute/v1/projects/centos-cloud/global/images')
+          .to_return(body: File.read(File.join(__dir__, '..', '..', 'fixtures', 'images_centos_cloud.json')))
+
+        subject.stub(:all_projects, %w[centos-cloud]) do
+          assert_equal %w[coastal-image centos-6], subject.images.map(&:name)
+        end
+      end
+
+      it 'list images with filter' do
+        not_found_body = File.read(File.join(__dir__, '..', '..', 'fixtures', 'images_nothing_found.json'))
+        %w[coastal-haven-123456 centos-cloud].each do |project|
+          url = "https://compute.googleapis.com/compute/v1/projects/#{project}/global/images?filter=name%20=%20%22NOTHING_FOUND%22"
+          stub_request(:get, url).to_return(body: not_found_body)
+        end
+
+        subject.stub(:all_projects, %w[centos-cloud]) do
+          assert_empty subject.images(filter: 'name = "NOTHING_FOUND"')
+        end
+      end
+
+      it 'ignore deprecated images' do
+        stub_request(:get, 'https://compute.googleapis.com/compute/v1/projects/deprecated/global/images')
+          .to_return(body: File.read(File.join(__dir__, '..', '..', 'fixtures', 'images_deprecated.json')))
+
+        subject.stub(:all_projects, %w[deprecated]) do
+          assert_equal ['coastal-image'], subject.images.map(&:name)
+        end
+      end
+    end
   end
 end
