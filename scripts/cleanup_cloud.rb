4f40d7ed183f0e1ea665e5773ae3e00d0da80f9b
Leos Stejskal <lstejska@redhat.com>
2022-05-25 14:53:05 +0200

    Fix error handling

--- scripts/cleanup_cloud.rb
+++ scripts/cleanup_cloud.rb
@@ -1,35 +1,55 @@
-require_relative '../lib/foreman_google/google_compute_adapter'
+require 'json'
+require 'google-cloud-compute'
 
 path = ARGV[0]
-zone = ARGV[1]
+@zone = ARGV[1]
 
-if path.nil? || zone.nil?
+if path.nil? || @zone.nil?
   puts 'Missing argument path or zone'
   puts "Run script like this: bundle exec ruby scripts/cleanup_cloud.rb 'path/to/json' 'zone_name'"
   exit 1
 end
 
-client = ForemanGoogle::GoogleComputeAdapter.new auth_json_string: File.read(path)
+@auth_json = JSON.parse(File.read(path))
 
-instances = client.instances(zone)
+def resource_client(resource_name)
+  ::Google::Cloud::Compute.public_send(resource_name) do |config|
+    config.credentials = @auth_json
+  end
+end
 
-puts 'Found instances:'
+def list(resource_name)
+  response = resource_client(resource_name).list(project: @auth_json['project_id'], zone: @zone).response
+  response.items
+rescue ::Google::Cloud::Error => e
+  puts "ERROR: #{e}"
+end
+
+def delete(resource_name, **opts)
+  resource_client(resource_name).delete(project: @auth_json['project_id'], zone: @zone, **opts)
+rescue ::Google::Cloud::Error => e
+  puts "ERROR: #{e}"
+end
+
+instances = list('instances')
+
+puts 'Deleting instances:'
 puts 'Nothing found' if instances.empty?
 
 instances.each do |instance|
   puts ">>> Deleting #{instance.name} instance"
-  client.delete_instance(zone, instance.name)
+  delete('instances', instance: instance.name)
 end
 
-disks = client.disks(zone)
+disks = list('disks')
 
 puts ''
-puts 'Found disks:'
+puts 'Deleting disks:'
 puts 'Nothing found' if disks.empty?
 
 disks.each do |disk|
   puts ">>> Deleting #{disk.name} disk"
-  client.delete_disk(zone, disk.name)
+  delete('disks', disk: disk.name)
 end
 
 puts ''
